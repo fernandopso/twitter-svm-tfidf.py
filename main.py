2f1e42b003f332e7cbf60d92d685d69c8f8949f3
Fernando Oliveira <fernandopso3@gmail.com>
2015-10-24 20:22:32 -0200

    Rename mining to miner

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from cli import Cli
 from storage import Storage
 from collector.collect import Collect
 from training import Training
-from mining.mining import Mining
+from miner.mining import Mining
 
 if __name__ == '__main__':
     cli = Cli()
--- miner/mining.py
+++ miner/mining.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from models import Models
 from tfidf import TermFrequency
 from vectorizer import Vectorizer
-from models import Models
 from training import Training
 
 class Mining(object):
