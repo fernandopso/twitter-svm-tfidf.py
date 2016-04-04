04caf567b2638a6fb4cc4cf1d17d8e95a695aff4
Fernando <fernandopso3@gmail.com>
2016-04-04 18:11:46 -0300

    storage class refactoring

--- app/storage.py
+++ app/storage.py
@@ -5,32 +5,15 @@ from time import gmtime, strftime
 from json import dump, loads
 
 class Storage(object):
-    """
-    Save and open files
-
-    required params:
-        @files is a list of json for *save* or *load*
-
-        @output_type is a type for *save* or *load* from data folder
-            Paths can be accessible by key *collected* or *trained* in FOLDERS
-
-            Ex.: Storage([{"a":"z"}], 'collected').save()
-                 Storage([{"a":"z"}], 'trained').save()
-
-                 Storage([], 'trained').load()
-                 Storage([], 'collected').load()
-    """
-
     FOLDERS = {
         'collected': './data/collected/',
         'trained': './data/trained/'
     }
 
-    def __init__(self, files, output_type):
-        self.files = files
+    def __init__(self, output_type):
         self.output_type = Storage.FOLDERS[output_type]
 
-    def save(self):
+    def save(self, files):
         """"
         Save file of json in file with name like 2015_09_30
         """
@@ -40,7 +23,7 @@ class Storage(object):
 
         outfile = open(f, 'a')
 
-        for json in self.files:
+        for json in files:
             if type(json) is dict:
                 dump(json, outfile)
             else:
@@ -58,11 +41,13 @@ class Storage(object):
         """
         Restore all files collected or trained and return a json of tweets
         """
+        files = []
+
         folder = listdir(self.output_type)
 
         for file_name in folder:
             with open(self.output_type + file_name, 'r') as data:
                 for tweet in data.readlines():
-                    self.files.append(loads(tweet))
+                    files.append(loads(tweet))
 
-        return self.files
+        return files
--- hmi.py
+++ hmi.py
@@ -27,55 +27,55 @@ if __name__ == '__main__':
                 cli.error(cli.option)
             else:
                 tweets = Collect(cli.args[0]).search()
-                Storage(tweets, 'collected').save()
+                Storage('collected').save(tweets)
                 cli.success(cli.option)
 
         elif cli.option == 't':
             # Load files
-            data = Storage([], 'collected').load()
+            data = Storage('collected').load()
 
             # Training of tweets
             tweets = Training(data).evaluate()
 
             # Save tweets
-            Storage(tweets, 'trained').save()
+            Storage('trained').save(tweets)
 
         elif cli.option == 'p':
-            collect_files = Storage([], 'collected').load()
+            collect_files = Storage('collected').load()
 
-            trained_files = Storage([], 'trained').load()
+            trained_files = Storage('trained').load()
             trained_files = Training(trained_files).process_tweets()
 
             Mining(collect_files, trained_files).start()
 
         # TODO: Use command options and arguments
         elif cli.option == 'tweets':
-            data = Storage([], 'collected').load()
+            data = Storage('collected').load()
             cli.tweets_colleted(data)
 
         elif cli.option == 'tweets trained':
-            data = Storage([], 'trained').load()
+            data = Storage('trained').load()
             cli.tweets_trained(data)
 
         elif cli.option == 'tweets trained positive':
-            data = Storage([], 'trained').load()
+            data = Storage('trained').load()
             cli.tweets_trained(data, '1')
 
         elif cli.option == 'tweets trained negative':
-            data = Storage([], 'trained').load()
+            data = Storage('trained').load()
             cli.tweets_trained(data, '2')
 
         elif cli.option == 'tweets trained neutral':
-            data = Storage([], 'trained').load()
+            data = Storage('trained').load()
             cli.tweets_trained(data, '3')
 
         elif cli.option == 'tweets trained unknown':
-            data = Storage([], 'trained').load()
+            data = Storage('trained').load()
             cli.tweets_trained(data, '4')
 
         elif cli.option == 'tweets metrics':
-            collected = Storage([], 'collected').load()
-            trained   = Storage([], 'trained').load()
+            collected = Storage('collected').load()
+            trained   = Storage('trained').load()
             cli.tweets_metrics(collected, trained)
 
         cli.waiting_input()
